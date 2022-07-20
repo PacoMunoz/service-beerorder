d9cbae761d54159e14b9b1f13571afd567479dc7
PacoMunoz <iicc96@hotmail.com>
2022-07-20 05:44:45 +0200

    Lombok annotations

--- src/main/java/com/pmg/brewery/model/BeerInventoryDto.java
+++ src/main/java/com/pmg/brewery/model/BeerInventoryDto.java
@@ -1,6 +1,10 @@
 package com.pmg.brewery.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
@@ -9,6 +13,10 @@ import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BeerInventoryDto implements Serializable {
 
     private static final long serialVersionUID = 4578399603011178803L;
